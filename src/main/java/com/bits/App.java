95ed7641b6ca897fb984a00b06214b3a21b1d694
TusharMate_2022wb86122 <2022wb86122@wilp.bits-pilani.ac.in>
2026-09-25 00:34:33 +0530

    Test continuous integration trigger

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -4,6 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("BITS DevOps Lab - Jenkins Maven Build");
-        System.out.println("Student ID: 2022wb86122");
+        System.out.println("Student ID: 2022wb86803");
+        System.out.println("Continuous Integration Test - Build Triggered Successfully");
     }
 }
